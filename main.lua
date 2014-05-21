47981d6c4780b68b7b4b7f4c55d438000c2bb41b
Yamano Lemon <MountainLemon@gmail.com>
2014-05-21 01:20:01 -0400

    Print grid content

--- main.lua
+++ main.lua
@@ -1,20 +1,50 @@
 
+
 function love.load()
 
 	-- Set window sizen
-	tile_size = 128;
-	love.window.setMode(tile_size * 4, tile_size * 4, 
+	tileSize = 128;
+	love.window.setMode(tileSize * 4, tileSize * 4, 
 						{resizable=false, vsync=false } )
 
 	-- get Window Dimension
-	win_width, win_height = love.graphics.getDimensions( )
+	winWidth, winHeight = love.graphics.getDimensions( )
+	love.graphics.setColor(255,255,255)
 
+
+
+	grid = {}
+
+	count = 2
+	for row = 1, 4 do
+		table.insert(grid, {})
+		for col = 1, 4 do
+			table.insert(grid[row], count)
+			-- count = count + 1
+		end
+	end
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+	-- draw cells
+	for row, currRow in ipairs(grid) do
+		for col, currCell in ipairs(currRow) do
+			if currCell ~= nil then
+				love.graphics.print(currCell, (row - 1) * tileSize, (col - 1) * tileSize, 0, 8, 8 )
+			end
+		end
+	end
+
+	-- Draw gridline
+	for loc_x = 0, winWidth, tileSize do
+		love.graphics.line(loc_x, 0, loc_x, winHeight)
+	end
+	for loc_y = 0, winHeight, tileSize do
+		love.graphics.line(0 ,loc_y, winWidth, loc_y)
+	end
 end
 
 function love.keypressed( key, isrepeat)
